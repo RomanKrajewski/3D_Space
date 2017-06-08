d78fff9e99d4e9ac2dd864d52f90b1a79ae071d6
romankrajewski <s0553506@htw-berlin.de>
2017-06-08 17:55:38 +0200

    add podests

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ animate();
 
 function init() {
 
-    var  mesh, light;
+    var  mesh, hemisphereLight;
 
 
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
@@ -41,17 +41,52 @@ function init() {
     mesh = new THREE.Mesh( new THREE.CubeGeometry(1000, 1000, 1000 ), materials);
 
     mesh.scale.x = - 1;
-    var standardMaterial = new THREE.MeshStandardMaterial( {color:0xE80C7A, roughness:0.3, metalness:0.7} );
+    var standardMaterial = new THREE.MeshStandardMaterial( {color:0xFFFFFF, roughness:1, metalness:0.6} );
     // platform = new THREE.Mesh(new THREE.CubeGeometry(20, 1, 20), standardMaterial);
     // platform.position.y = -5;
-    var platform = new THREE.Mesh(new THREE.ExtrudeGeometry(generatePentagon(10), {amount:1}), standardMaterial);
-    platform.position.z = -50;
+    var platform = new THREE.Mesh(new THREE.ExtrudeGeometry(generatePentagon(40), {amount:0.5}), standardMaterial);
+    var podest = new THREE.Mesh(new THREE.ExtrudeGeometry(generatePentagon(20), {amount:0.5}), standardMaterial);
+    var podest2 = new THREE.Mesh(new THREE.ExtrudeGeometry(generatePentagon(8), {steps: 1, amount:13, bevelEnabled:false}), standardMaterial);
+    podest.rotation.z = toRadians(180);
+    podest.rotation.x = toRadians(90);
+    podest.position.y = -20;
+    podest.position.z = 45;
+    podest.position.x = -15;
+    // podest2.rotation.z = toRadians(180);
+    podest2.rotation.x = toRadians(90);
+    podest2.position.y = -2;
+    podest2.position.z = 45;
+    podest2.position.x = -15;
+    podest2.castShadow = true;
+    podest.receiveShadow = true;
+    platform.rotation.x = toRadians(90);
+    platform.position.y = -20;
     scene.add( mesh );
     scene.add(platform);
+    scene.add(podest);
+    scene.add(podest2);
     // scene.background = mesh;
     // light = new THREE.HemisphereLight(0xfefdc4, 0x7f848a,  1);
-    light = new THREE.HemisphereLight(0xfefdc4, 0xfefdc4,  1);
-    scene.add(light);
+    hemisphereLight = new THREE.HemisphereLight(0xFFFFFF, 0xFFFFFF,  1);
+    spotLight = new THREE.SpotLight(0xfefdc4,1,20, toRadians(90), 0,0);
+    spotLight.position.z = 5;
+    spotLight.position.y = 10;
+    spotLight.position.x = 20;
+    spotLight.castShadow = true;
+    spotLight.shadowCameraVisible = true;
+    var d = 200;
+
+    spotLight.shadowCameraLeft = -d;
+    spotLight.shadowCameraRight = d;
+    spotLight.shadowCameraTop = d;
+    spotLight.shadowCameraBottom = -d;
+
+    spotLight.shadowCameraFar = 1000;
+    spotLight.shadowDarkness = 0.2;
+
+    scene.add(hemisphereLight);
+    scene.add(spotLight);
+    spotLight.target = platform;
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
@@ -59,30 +94,26 @@ function init() {
     orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
 
     document.getElementById("canvas_wrapper").appendChild( renderer.domElement );
-    camera.position.y = 1 ;
+    camera.position.y = 0.1 ;
 
     fingercontrols = document.getElementById("fingercontrolswitch");
-
-
 }
 
 
 function loadTexture( path ) {
-
     var texture = new THREE.Texture( );
     var material = new THREE.MeshBasicMaterial( { map: texture} );
-
     var image = new Image();
     image.onload = function () {
-
         texture.image = this;
         texture.needsUpdate = true;
-
     };
     image.src = path;
-
     return material;
+}
 
+function toRadians(deg){
+    return deg*(Math.PI/180)
 }
 
 function generatePentagon(radius){
@@ -91,7 +122,7 @@ function generatePentagon(radius){
     shape.moveTo(radius, 0);
     var angle = 0;
     for(var i = 0; i<4; i++){
-        angle += (72*(Math.PI/180));
+        angle += toRadians(72);
         shape.lineTo(radius*Math.cos(angle), radius*Math.sin(angle));
     }
     shape.lineTo(radius,0);
@@ -99,15 +130,12 @@ function generatePentagon(radius){
 }
 
 function animate() {
-
     requestAnimationFrame( animate );
     update();
-
 }
 
 
 function update() {
-
     renderer.render( scene, camera );
     if(fingercontrols.checked){
         orbitControls.enabled = true;
@@ -118,9 +146,3 @@ function update() {
         orientationControls.update();
     }
 }
-
-// window.ondeviceorientation = function (event) {
-//     accelerationX = event.beta* (Math.PI / 180);
-//     accelerationY = event.gamma* (Math.PI / 180);
-//     accelerationZ = event.alpha* (Math.PI / 180);
-// }
